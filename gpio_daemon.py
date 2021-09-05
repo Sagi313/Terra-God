4b5cc06ed747df6ad66ba07757b49fe6cc235ebe
Sagi Buria <sagib313@gmail.com>
2021-09-05 22:59:22 +0300

    Update gpio_daemon.py

--- gpio_daemon.py
+++ gpio_daemon.py
@@ -24,26 +24,26 @@ def lights_handler(switches):
             GPIO.setup(light_pin, GPIO.OUT)
 
             if interval.start_time <= now and now <= interval.end_time:
-                GPIO.output(light_pin, GPIO.HIGH)
+                GPIO.output(light_pin, GPIO.LOW)
                 print(f"{interval.start_time} < {now} < {interval.end_time}")
 
             else:
                 GPIO.output(light_pin, GPIO.HIGH)
                 print(f"{interval.start_time} not {now} not {interval.end_time}")
     
-    elif switches.lights_switch == "on":    # Always on. Ignores the timer
-        print("by on switch")
+    elif switches.lights_switch == "off":    # Always off. Ignores the timer
+        print("by off switch")
         for interval in Light_interval.objects.all():
             light_pin = interval.pin_number
             GPIO.setup(light_pin, GPIO.OUT)
             GPIO.output(light_pin, GPIO.HIGH)
     
     else: # Switch off
-        print("by off switch")
+        print("by on switch")
         for interval in Light_interval.objects.all():
             light_pin = interval.pin_number
             GPIO.setup(light_pin, GPIO.OUT)
-            GPIO.output(light_pin, GPIO.HIGH)
+            GPIO.output(light_pin, GPIO.LOW)
 
 def fans_handler(switches):
     pass
